ae17d8a4568d0234ac4c5e0b5a0533055a5b6f01
aaronlrv <aaronvitharanage@gmail.com>
2024-11-17 02:15:27 +0400

    Change js button to match new html file

--- public/src/thankYou.js
+++ public/src/thankYou.js
@@ -1,4 +1,4 @@
-let dashBtn = document.getElementById("dashboard-button");
+let dashBtn = document.querySelector(".btn-29");
 
 dashBtn.addEventListener("click", () => {
   window.location.href = "/dashboard"; // Redirect to a confirmation page if needed
